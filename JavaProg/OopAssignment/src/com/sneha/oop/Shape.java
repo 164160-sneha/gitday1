3fd3dedb4f444da969468e096b8d8b84a48b9ddb
164160-sneha <44638784+164160-sneha@users.noreply.github.com>
2018-11-18 22:56:43 +0530

    Add files via upload

---
+++ JavaProg/OopAssignment/src/com/sneha/oop/Shape.java
@@ -0,0 +1,22 @@
+package com.sneha.oop;
+
+public class Shape {
+	abstract void draw();{
+		
+	}
+
+	public staticss void main(String[] args) {
+		
+		
+		Rectangle rectangle=new Rectangle();
+		rectangle.draw();
+		
+		Cube cube=new Cube();
+		cube.draw();
+		
+		Line lin=new Line();
+		lin.draw();
+		
+	}
+
+}
